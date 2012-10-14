270e5d5b4d8c38e97c864653c26ebbbbc5593a7b
TharinduRusira <tharindurusira@gmail.com>
2012-10-15 03:12:01 +0530

    LinearReg.py/readFile() reads data file successfully

--- LinearReg.py
+++ LinearReg.py
@@ -3,8 +3,8 @@ Created on Oct 15, 2012
 
 @author: M.P.Tharindu Rusira Kumara, University of Moratuwa
 '''
-import numpy;
-import sys;
+
+
 
 class LinearReg(object):
     '''
@@ -32,6 +32,7 @@ class LinearReg(object):
             
         '''
         
+        
         try:
             __fpath=filePath; # path of the file
             __size=fileSize; # number of data items in file
@@ -42,6 +43,12 @@ class LinearReg(object):
             __secondValStart=13;
             __secondValEnd=22;
             
+           
+            from numpy import zeros
+            from numpy import float64
+            
+            __array= zeros((__size,2),float64);
+            
             f=open(__fpath, mode='r', buffering=1, encoding=None, errors=None, newline=None, closefd=True); 
             
             print('reading data from file....');
@@ -51,13 +58,19 @@ class LinearReg(object):
                 __firstValue= line[__firstValStart:__firstValEnd];
                 __secondValue=line[__secondValStart:__secondValEnd];
                 
-                print(__firstValue+'  '+__secondValue);
+                #print(__firstValue+'  '+__secondValue);
+                __array[i,0]= __firstValue
+                __array[i,1]= __secondValue;
+                #print(__array[i,0],__array[i,1]);
              
             print('data reading complete....');
+            
+            return __array;
             
         except IOError:
             pass 
         
 lr=LinearReg();
-lr.readFile('data.txt',62);
+a=lr.readFile('data.txt',62);
+print(a[1,0],a[1,1]);
  
