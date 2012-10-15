f6fb90b5bb623f040c54452bbe729452f324fe3b
TharinduRusira <tharindurusira@gmail.com>
2012-10-15 14:43:04 +0530

    15.10.2012 2.42 P.M GMT +5.30

--- LinearReg.py
+++ LinearReg.py
@@ -3,6 +3,9 @@ Created on Oct 15, 2012
 
 @author: M.P.Tharindu Rusira Kumara, University of Moratuwa
 '''
+from numpy.core.numeric import zeros, arange, ones, dot
+from numpy.matrixlib.defmatrix import matrix
+
 
 
 
@@ -44,10 +47,12 @@ class LinearReg(object):
             __secondValEnd=22;
             
            
-            from numpy import zeros
+            
             from numpy import float64
             
-            __array= zeros((__size,2),float64);
+            # 3 columns. bias value, first value, second value 
+            
+            __array= ones((__size,3),float64);
             
             f=open(__fpath, mode='r', buffering=1, encoding=None, errors=None, newline=None, closefd=True); 
             
@@ -58,10 +63,10 @@ class LinearReg(object):
                 __firstValue= line[__firstValStart:__firstValEnd];
                 __secondValue=line[__secondValStart:__secondValEnd];
                 
-                #print(__firstValue+'  '+__secondValue);
-                __array[i,0]= __firstValue
-                __array[i,1]= __secondValue;
-                #print(__array[i,0],__array[i,1]);
+                
+                __array[i,1]= __firstValue
+                __array[i,2]= __secondValue;
+                #print(__array[i,1],__array[i,2]);
              
             print('data reading complete....');
             
@@ -70,7 +75,52 @@ class LinearReg(object):
         except IOError:
             pass 
         
-lr=LinearReg();
-a=lr.readFile('data.txt',62);
-print(a[1,0],a[1,1]);
- 
+        
+    def linReg(self,arr):
+        __matrix=arr;
+        
+        # public code goes here
+        
+    
+    
+    def __computeCost(self,mat,para=zeros((1,2))): 
+        # if para is not overridden with custom initial values, use zeros
+        # cost computation code goes here
+        
+        __cost=0.0;
+        __size= mat[:,1].size;
+        
+        for i in mat:
+            __cost=__cost + (dot(mat[i,0:2],para)- mat[i,2])**2;
+            
+            
+        __cost= __cost/(2*__size);
+        
+        return __cost;
+           
+    
+    def __gradientDecent(self,datamat,para=zeros((1,2)),learningRate=1,iterNum=500):
+        # optimization code goes here 
+        
+        __size= datamat[:,1].size;
+        __parameterVector= para;
+        
+        print("Gradient Descent is finding optimal parameters");
+        for i in range (0,iterNum):
+            __t0= __parameterVector[0];
+            __t1= __parameterVector[1];
+            
+            __t0= __t0 - (learningRate/__size)*(dot(datamat[i,0:2],__parameterVector)- datamat[i,2]);
+            __t1= __t1 - (learningRate/__size)* (dot(datamat[i,0:2],__parameterVector)- datamat[i,2])*datamat[i,1];
+            
+            __parameterVector[0]=__t0;
+            __parameterVector[1]=__t1;
+            
+        minPara= (__t0,__t1);
+        
+        print("Gradient Descent is complete");
+        return minPara;
+    
+          
+
+
--- python_README.txt
+++ python_README.txt
@@ -1,11 +1,25 @@
+@Author M.P. Tharindu Rusira Kumara, tharindurusira@gmail.com
+
 data resource 
 
 REGRESSION - Linear Regression Test Data
 http://orion.math.iastate.edu/burkardt/data/regression/regression.html
 
-data file format
- ------------------------
-| index value1 value2   |
-|                       |
--------------------------
+									data file format
+								 -----------------------
+								| index value1 value2   |
+								|                       |
+								 -----------------------
 
+	 ====================================================================================
+	|	The idea here is, we are trying to find an optimal linear decision boundary      |
+	|	which seperates a data set given that the data are linearly seperable.           |
+	|	                                                                                 |
+	|	Objective: find an optimal set of parameters t0,t1                               | 
+	|	           such that t0+ t1*X = Y separates the whole data set into two classes. |  
+	|																					 |
+    |   Procedure: We calculate the overall square cost over the training data set and   |
+	|	           minimize the cost using gradient decsent algorithm.					 | 
+    |                                                                 					 |
+	|	Outcome: the algorithm is capable of deciding the class of a new data point      |
+	|====================================================================================|
